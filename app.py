53f0a1f5a7d9c9c1931f3fd10602c683f934d508
Gabriel Andrade <gabrielandrade248@gmail.com>
2024-10-29 10:49:32 -0300

    Implementing function get all meals & adding comments

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
 db.init_app(app)
 
+#Criação de uma refeição no banco de dados.
 @app.route('/meal', methods=['POST'])
 def create_meal():
     data = request.json
@@ -26,6 +27,7 @@ def create_meal():
     
     return jsonify({"message": 'Não foi possível criar a refeição'}), 400
 
+#Lê as informações de uma refeição, com base no ID enviado pelo cliente.
 @app.route('/meal/<int:id_meal>', methods=['GET'])
 def get_meal_by_id(id_meal):
     meal = db.session.execute(db.select(Meal).filter_by(id=id_meal)).scalar_one()
@@ -33,6 +35,18 @@ def get_meal_by_id(id_meal):
     if meal:
         return {"name": meal.name, "description": meal.description, "date": meal.date, "hour": meal.hour, "inside_diet": meal.inside_diet}
 
+#Lê as informações de todas as refeições presentes no bando de dados.
+@app.route('/meal', methods=['GET'])
+def get_all_meal():
+    all_meals = []
+    meals = db.session.execute(db.select(Meal).order_by(Meal.id)).scalars()
+    
+    for meal in meals:
+        all_meals.append({"name": meal.name, "date": meal.date, "hour": meal.hour, "inside_diet": meal.inside_diet})
+    
+    return all_meals
+
+#Edita uma refeição escolhida pelo cliente com base no ID enviado.
 @app.route('/meal/<int:id_meal>', methods=['PUT'])
 def edit_meal(id_meal):
     data = request.json
@@ -57,6 +71,7 @@ def edit_meal(id_meal):
     
     return jsonify({"message": "Não foi possível editar a refeição"})
 
+#Deleta uma refeição com base no ID enviado pelo cliente
 @app.route('/meal/<int:id_meal>', methods=['DELETE'])
 def delete_meal(id_meal):
     meal = db.session.execute(db.select(Meal).filter_by(id=id_meal)).scalar_one()
@@ -65,10 +80,6 @@ def delete_meal(id_meal):
     db.session.commit()
 
     return jsonify({"message": f"A refeição {meal.name} foi deletada"})
-
-@app.route('/')
-def hello_world():
-    return 'Hello World'
 
 if __name__ ==  '__main__':
     app.run(debug=True)
--- models/meal.py
+++ models/meal.py
@@ -1,16 +1,15 @@
 from database import db
-from datetime import datetime, timezone
 
 class Meal(db.Model):
     __tablename__ = 'meals'  # Nome da tabela no banco de dados
     
     # Definição das colunas
     id = db.Column(db.Integer, primary_key=True)  # Chave primária
-    name = db.Column(db.String(100), nullable=False)  # Nome obrigatório
-    description = db.Column(db.String(255), nullable=True)  # Descrição opcional
-    date = db.Column(db.String(10), nullable=False) # Data
-    hour = db.Column(db.String(5), nullable=False)
-    inside_diet = db.Column(db.Boolean, nullable=True, default=False)  # Booleano obrigatório
+    name = db.Column(db.String(100), nullable=False)  # Nome da refeição
+    description = db.Column(db.String(255), nullable=True)  # Descrição da refeição
+    date = db.Column(db.String(10), nullable=False) # Data da refeição
+    hour = db.Column(db.String(5), nullable=False) # Hora da refeição
+    inside_diet = db.Column(db.Boolean, nullable=True, default=False)  # Está ou não dentro da dieta
 
     # Construtor da classe
     def __init__(self, name, date, hour, description=None, inside_diet=False):
